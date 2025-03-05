c11af2604653b6c29cccb0b7bca62cba7ca1f17e
David Thomas <me@dt.id.au>
2025-03-06 00:09:27 +0800

    conig

--- custom_components/mitsubishi_heavy_ac/const.py
+++ custom_components/mitsubishi_heavy_ac/const.py
@@ -6,3 +6,16 @@ DEFAULT_NAME: Final = "Mitsubishi Heavy AC"
 CONF_MIN_TEMP: Final = "min_temp"
 CONF_MAX_TEMP: Final = "max_temp"
 CONF_REMOTE: Final = "remote"
+
+# Configuration options
+CONF_TEMPERATURE_SENSOR: Final = "temperature_sensor"
+CONF_HUMIDITY_SENSOR: Final = "humidity_sensor"
+
+# Valid configuration keys
+VALID_CONFIG_KEYS: Final = {
+    CONF_TEMPERATURE_SENSOR,
+    CONF_HUMIDITY_SENSOR,
+    CONF_MIN_TEMP,
+    CONF_MAX_TEMP,
+    CONF_REMOTE,
+}
